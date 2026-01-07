d1ce20eda69b13a4b28a91fddc124d049e093479
Rajesh Mondal <rajesh01749@gmail.com>
2026-01-08 01:46:33 +0600

    feat(i18n): implement session-based language switching

---
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class LocaleSubscriber implements EventSubscriberInterface
+{
+    // private string $defaultLocale;
+    private string $defaultLocale = 'en';
+
+    public function __construct()
+    {
+        // $this->defaultLocale = 'en';
+    }
+
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        $request = $event->getRequest();
+
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
+
+        if ($locale = $request->getSession()->get('_locale')) {
+            $request->setLocale($locale);
+        } else {
+            $request->setLocale($this->defaultLocale);
+        }
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::REQUEST => [['onKernelRequest', 20]],
+        ];
+    }
+}
